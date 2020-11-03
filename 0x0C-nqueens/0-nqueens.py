51e8f30124110357ab5b49fb6f65e4198548c3c2
auraPasmin <1288@holbertonschool.com>
2020-11-03 18:41:04 -0500

    fix

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,60 +1,121 @@
 #!/usr/bin/python3
+
 import sys
 
 
-def queens(n):
-    paths = []
-    data_set = set()
-    for column in range(n):
-        paths.append([0, column])
-        data_set.add(column)
+def diagonals(results, N):
 
-    track = []
+    # fill diagonals
+    diagonals = []
+    for i in results:
+        # up-left
+        it_row = i[0]
+        it_col = i[1]
+        while it_col >= 0 and it_col < N and it_row >= 0 and it_row < N:
+            if [it_row, it_col] not in diagonals:
+                diagonals.append([it_row, it_col])
+            it_row -= 1
+            it_col -= 1
 
-    while paths:
-        [row, column] = paths.pop(0)
-        while track and (row < track[0][0]):
-            track.pop(0)
-        if track and (row == track[0][0]):
-            track[0] = [row, column]
-        else:
-            track.insert(0, [row, column])
+        # up-right
+        it_row = i[0]
+        it_col = i[1]
+        while it_col >= 0 and it_col < N and it_row >= 0 and it_row < N:
+            if [it_row, it_col] not in diagonals:
+                diagonals.append([it_row, it_col])
+            it_row -= 1
+            it_col += 1
 
-        nextRow = row + 1
+        # up-right
+        it_row = i[0]
+        it_col = i[1]
+        while it_col >= 0 and it_col < N and it_row >= 0 and it_row < N:
+            if [it_row, it_col] not in diagonals:
+                diagonals.append([it_row, it_col])
+            it_row += 1
+            it_col -= 1
 
-        data_death_zone = set()
-        for (r, c) in track:
-            data_death_zone.add(c)
-            distance = nextRow - r
-            if c - distance >= 0:
-                data_death_zone.add(c - distance)
-            if c + distance < n:
-                data_death_zone.add(c + distance)
+        # down-right
+        it_row = i[0]
+        it_col = i[1]
+        while it_col >= 0 and it_col < N and it_row >= 0 and it_row < N:
+            if [it_row, it_col] not in diagonals:
+                diagonals.append([it_row, it_col])
+            it_row += 1
+            it_col += 1
 
-        localSafeZone = data_set.difference(data_death_zone)
-        if not localSafeZone:
-            if nextRow == n:
-                copy = track.copy()
-                copy.reverse()
-                print(copy, flush=True)
-            track.pop(0)
-        else:
-            localSafeZone = list(localSafeZone)
-            localSafeZone.reverse()
-            for position in localSafeZone:
-                paths.insert(0, [nextRow, position])
+    return diagonals
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
+def isSafe(row, col, results, N):
+    """
+    know if safe a position
+    """
+    # validate columns
+    for _row in range(N):
+        if [_row, col] in results:
+            return False
+
+    return not [row, col] in diagonals(results, N)
+
+
+def chess(N):
+    """
+    iterate the positions
+    """
+    result = []
+    row = 0
+    col = 0
+
+    while row < N:
+        while col < N:
+            if isSafe(row, col, result, N):
+                result.append([row, col])
+                break
+            col += 1
+
+        # if a new position not exists in results
+        if len(result) != (row + 1):
+            row -= 1
+            if row < 0:
+                break
+            col = result[row][1] + 1
+            del result[row]
+            continue
+        elif len(result) == N:
+            print(result)
+            col += 1
+            del result[row]
+            continue
+        row += 1
+        col = 0
+
+
+def start():
+    """
+    N queens
+    """
+    args = sys.argv
+
+    # Usage error
+    if len(args) is not 2:
         print("Usage: nqueens N")
-        exit(1)
-    elif int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
+
+    # type value error
     try:
-        n = int(sys.argv[1])
+        int(args[1])
     except:
         print("N must be a number")
-        exit(1)
-    queens(n)
+        sys.exit(1)
+
+    # less than 4 error
+    if int(args[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    N = int(args[1])
+    chess(N)
+
+if __name__ == "__main__":
+    start()
